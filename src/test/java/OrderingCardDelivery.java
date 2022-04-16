8b2c593d4bc8fbf2c23c65d8e5856f8d38202b6a
Artem Sukhanov <IceMan161@yandex.ru>
2022-04-16 16:26:47 +0300

    mod bg , src

--- src/test/java/OrderingCardDelivery.java
+++ src/test/java/OrderingCardDelivery.java
@@ -1,11 +1,9 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.*;
@@ -20,8 +18,8 @@ public class OrderingCardDelivery {
 
         open("http://localhost:9999/");
         $x("//input[@type='text']").val("Ростов-на-Дону");
-        $("[data-test-id=date] input").sendKeys(chord(COMMAND, "a"), DELETE);
-        $("[data-test-id=date] input").sendKeys("20.04.2022");
+        $("[data-test-id='date'] input").sendKeys(chord(COMMAND, "a"), DELETE);
+        $("[data-test-id='date'] input").setValue("20.04.2022");
         $x("//input[@name='name']").val("Суханов Артем");
         $x("//input[@name='phone']").val("+79612333444");
         $x("//label[@data-test-id='agreement']").click();
